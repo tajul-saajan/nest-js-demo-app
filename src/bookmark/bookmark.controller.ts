0e68a26a039f93125bdc6bf4e0e216d2911f3be7
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-04-13 02:25:23 +0600

    bookmark(crud): added update added delete added query param

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,16 +1,41 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { UpdateBookmarkDto } from './dto/update-bookmark.dto';
 @Controller('bookmarks')
 export class BookmarkController {
   constructor(private readonly service: BookmarkService) {}
   @Get('/')
-  index() {
-    return this.service.findAll();
+  async index(@Query('title') title: string, @Query('desc') desc: string) {
+    return await this.service.findAll(title, desc);
   }
 
   @Post('/')
-  store(@Body() dto: CreateBookmarkDto) {
-    return this.service.create(dto);
+  async store(@Body() dto: CreateBookmarkDto) {
+    return await this.service.create(dto);
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: number) {
+    return await this.service.show(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() dto: UpdateBookmarkDto) {
+    return await this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id') id: number) {
+    return await this.service.destroy(id);
   }
 }
